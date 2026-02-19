3d9fed02e21b0ad6407b3ec3cb1d924d7e424222
김형준 <hyungjun@vyv.xyz>
2026-02-19 11:20:14 +0900

    comment out

--- main.ts
+++ main.ts
@@ -1,28 +1,23 @@
 import "dotenv/config";
-import { fillInstagramProfile } from "./src/tasks/fill-instagram-profile";
-import { fillYoutubeProfile } from "./src/tasks/fill-youtube-profile";
-import { fillFeaturings } from "./src/tasks/fill-featurings";
-import { fillInstagramContents } from "./src/tasks/fill-instagram-contents";
-import { fillYoutubeContents } from "./src/tasks/fill-youtube-contents";
 
 const task = process.env["TASK_TO_RUN"] || "all";
 
 console.log(`🚀 Running task: ${task}`);
 
 try {
-  if (task === "profile" || task === "all") {
-    await fillInstagramProfile({ is_planning: false });
-    await fillYoutubeProfile({ is_planning: false });
-  }
+  // if (task === "profile" || task === "all") {
+  //   await fillInstagramProfile({ is_planning: false });
+  //   await fillYoutubeProfile({ is_planning: false });
+  // }
 
-  if (task === "featurings" || task === "all") {
-    await fillFeaturings();
-  }
+  // if (task === "featurings" || task === "all") {
+  //   await fillFeaturings();
+  // }
 
-  if (task === "contents" || task === "all") {
-    await fillInstagramContents();
-    await fillYoutubeContents();
-  }
+  // if (task === "contents" || task === "all") {
+  //   await fillInstagramContents();
+  //   await fillYoutubeContents();
+  // }
 
   console.log("✨ All selected tasks completed.");
 } catch (error) {
